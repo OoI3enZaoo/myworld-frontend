35e7523e0e56b25c8cd1c779d29691d1a2aa8e42
Theerapat <blend.theerapat@gmail.com>
2021-10-10 16:04:44 +0700

    style: footer

--- layouts/index.js
+++ layouts/index.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 const MainLayout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   )
 }
